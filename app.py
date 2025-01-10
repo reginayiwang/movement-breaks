0dc18e946be5f7e461a63a55eeec15383a35a8d2
Regina Wang <reginayiwang@gmail.com>
2025-01-10 12:48:19 -0800

    Implemented basic timer

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Flask app for Movement Breaks (productivity timer with guided exercise breaks)"""
 
-from flask import Flask
+from flask import Flask, render_template
 from models import db, connect_db
 
 app = Flask(__name__)
@@ -10,3 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+@app.route('/')
+def show_timer():
+    return render_template('timer.html') 
